cf5eee9b2f39a9c01dd38ea2f670f5153bf7f6e5
Adam Gehring <geh12001@byui.edu>
2018-01-28 20:35:15 -0700

    cart Stuff

--- web/shoppingCart.php
+++ web/shoppingCart.php
@@ -17,7 +17,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" >
 
     <!-- Custom styles for this template -->
-    <link rel="stylesheet" type="text/css" href="/css/homeStlye.css">
+    <link rel="stylesheet" type="text/css" href="/css/goodStyle.css">
 </head>
 
 <body>
