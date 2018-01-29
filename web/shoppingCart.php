468b19e48c4363076b02c5aa6a94dfa983317433
Adam Gehring <geh12001@byui.edu>
2018-01-28 20:02:26 -0700

    cart Stuff

--- web/shoppingCart.php
+++ web/shoppingCart.php
@@ -58,10 +58,27 @@
                 ?>
             </table>
         </div>
-        <div>
-            
-            <button class="btn btn-outline-success my-2 my-sm-0" id="cartButton" type="button" onclick="location.href='viewCart.php'"><img src="images/box.svg" alt="Shopping Cart"><div id="cartNum">0</div></button>
-     </div>
+        <nav class="navbar navbar-expand-md navbar-light navColor fixed-bottom">
+        <a class="navbar-brand" href="../index.html"><img class="logo" src="../ocean.png" alt="Ocean Picture"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="../index.html">Home<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Shopping</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./viewCart.php">Shopping Cart</a>
+                </li>
+            </ul>
+               <button class="btn btn-outline-success my-2 my-sm-0" id="cartButton" type="button" onclick="location.href='./viewCart.php'"><img src="images/box.svg" alt="Shopping Cart"><div id="cartNum">0</div></button>
+        </div>
+    </nav>
     </main>
     <!-- /.container -->
 
