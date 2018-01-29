2031df6da7b0f964560894e545672d3343a4cf6a
Adam Gehring <geh12001@byui.edu>
2018-01-28 20:44:12 -0700

    cart Stuff

--- web/shoppingCart.php
+++ web/shoppingCart.php
@@ -50,15 +50,23 @@
                     }
                 ?>
             </table>
+            <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <!-- <a class="nav-link" href="./viewCart.php">Shopping Cart</a> -->
+                </li>
+            </ul>
+               <button class="btn btn-outline-success my-2 my-sm-0" id="cartButton" type="button" onclick="location.href='viewCart.php'"><span>View Cart</span><div id="cartNum">0</div></button>
+        </div>
         </div>
         <nav class="navbar navbar-expand-md navbar-light navColor fixed-bottom">
         <div class="collapse navbar-collapse" id="navbarsExampleDefault">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="./viewCart.php">Shopping Cart</a>
+                    <!-- <a class="nav-link" href="./viewCart.php">Shopping Cart</a> -->
                 </li>
             </ul>
-               <button class="btn btn-outline-success my-2 my-sm-0" id="cartButton" type="button" onclick="location.href='viewCart.php'"><img src="images/box.svg" alt="Shopping Cart"><div id="cartNum">0</div></button>
+               <button class="btn btn-outline-success my-2 my-sm-0" id="cartButton" type="button" onclick="location.href='viewCart.php'"><span>View Cart</span><div id="cartNum">0</div></button>
         </div>
     </nav>
     </main>
