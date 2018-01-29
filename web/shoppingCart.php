b6d1247420917eaff3e47a02e9bdd0498bf6bd76
Adam Gehring <geh12001@byui.edu>
2018-01-28 19:58:35 -0700

    cart Stuff

--- web/shoppingCart.php
+++ web/shoppingCart.php
@@ -25,12 +25,12 @@
     
 <body>
 
-    
+    <nav>
         <div class="collapse navbar-collapse" id="navbarsExampleDefault">
             
                <button class="btn btn-outline-success my-2 my-sm-0" id="cartButton" type="button" onclick="location.href='./viewCart.php'"><img src="images/box.svg" alt="Shopping Cart"><div id="cartNum">0</div></button>
         </div>
-    
+    </nav>
 
     <main role="main" class="container">
 
@@ -73,7 +73,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     
-    <script type="text/javascript" src="shopping.js"></script>
+    <script type="text/javascript" src="/js/shopping.js"></script>
 
 </body>
 </html>
