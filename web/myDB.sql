622d17d81e0e754835c6a3bf8cd1e33cfe836651
Adam Gehring <geh12001@byui.edu>
2018-02-11 22:39:11 -0700

    databaseSetup

---
+++ web/myDB.sql
@@ -0,0 +1,26 @@
+
+CREATE TABLE users (
+  id int(11) NOT NULL,
+  user_name varchar(50) NOT NULL,
+  score int(11) NOT NULL,
+  category_id int(11) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE categories(
+  id int(11),
+  category_name varchar(50) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE questions (
+  id int(11) NOT NULL,
+  question_name text NOT NULL,
+  answer1 varchar(250) NOT NULL,
+  answer2 varchar(250) NOT NULL,
+  answer3 varchar(250) NOT NULL,
+  answer4 varchar(250) NOT NULL,
+  answer varchar(250) NOT NULL,
+  category_id int(11) NOT NULL,
+  PRIMARY KEY (id)
+);
